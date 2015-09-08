e31a6f622118f3f3fb43d1d603cb15db54dbdc48
Kieran Hunt <kieran.hunt92@gmail.com>
2015-09-08 10:33:37 +0200

    Adding a second command line argument for the number of lines to output.

--- pcapsizer.py
+++ pcapsizer.py
@@ -6,6 +6,8 @@ reader = pcapy.open_offline(sys.argv[1])
 count = 0
 
 while True:
+    if count >= int(sys.argv[2]):
+        break
     try:
         (header, payload) = reader.next()
 	print(str(count) + "," + str(header.getlen()))
